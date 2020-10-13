4702834f2d61d72980571bdc4beffbae2ffe8a6d
Pete Du Beau <peterdubeau@yahoo.com>
2020-10-13 11:43:11 -0400

    switched to posgres

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  default_scope { order(initiative: :desc) }
   # secondary_scope { order (id: :desc)}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_09_10_214529) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.string "code", limit: 5
     t.datetime "created_at", precision: 6, null: false
