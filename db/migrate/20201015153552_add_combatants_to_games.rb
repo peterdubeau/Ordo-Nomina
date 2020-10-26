11f8435ccf713ed7e68b25f51e4c0b434c0206f2
Pete Du Beau <peterdubeau@yahoo.com>
2020-10-26 13:52:30 -0400

    Remove 'tags' type from migration

--- db/migrate/20201015153552_add_combatants_to_games.rb
+++ db/migrate/20201015153552_add_combatants_to_games.rb
@@ -1,5 +1,5 @@
 class AddCombatantsToGames < ActiveRecord::Migration[6.0]
   def change
-    add_column :games, :combatants, :tags, array: true, default: []
+    add_column :games, :combatants, array: true, default: []
   end
 end
