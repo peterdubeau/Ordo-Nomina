4ce86d260d4cd675cdf81cdc0514b6003d910970
Pete Du Beau <peterdubeau@yahoo.com>
2020-10-30 16:05:07 -0400

    removed old migrations

--- db/migrate/20200910214529_rename_game_columns.rb
+++
@@ -1,4 +0,0 @@
-class RenameGameColumns < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
--- db/migrate/20201022182949_change_combatants_data_type.rb
+++
@@ -1,5 +0,0 @@
-class ChangeCombatantsDataType < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :games, :combatants
-  end
-end
