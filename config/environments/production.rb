147048af9b109376b2638902d2f4afe1bc7ebade
Pete Du Beau <peterdubeau@yahoo.com>
2020-11-03 14:33:46 -0500

    fixed typo in backend url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   config.web_socket_server_url = 'wss://live-initiative-tracker.herokuapp.com/cable'
 
-  config.action_cable.allowed_request_origins = [ 'https://yielding-punishment.surge.sh', 'http://yielding-punishment.surge.sh', "https://ENV["TRACKER_URL"]", "http://ENV["TRACKER_URL"]" ]
+  config.action_cable.allowed_request_origins = [ 'https://yielding-punishment.surge.sh', 'http://yielding-punishment.surge.sh', "https://#{ENV[TRACKER_URL]}", "http://#{ENV[TRACKER_URL]}" ]
   
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
