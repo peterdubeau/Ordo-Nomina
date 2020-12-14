fd73c5c45c0980577aaaf1b94f085deae58d7d4a
Pete Du Beau <peterdubeau@yahoo.com>
2020-12-14 10:25:02 -0500

    all notifications denied

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom'
 import CreateRoom from './Components/CreateRoom/CreateRoom'
 import JoinRoom from './Components/JoinRoom/JoinRoom'
@@ -9,6 +9,60 @@ import './App.css';
 
 export default function App (props) {
  
+  const [ permit, setPermit ] = useState()
+
+  Notification.requestPermission().then(function(result) {
+    console.log(result);
+  });
+
+  function askNotificationPermission() {
+    // function to actually ask the permissions
+    function handlePermission(permission) {
+      // Whatever the user answers, we make sure Chrome stores the information
+      if(!('permission' in Notification)) {
+        Notification.permission = permission;
+      }
+  
+      // set the button to shown or hidden, depending on what the user answers
+      if(Notification.permission === 'denied' || Notification.permission === 'default') {
+        setPermit({display: 'block'});
+      } else {
+        setPermit({display: 'none'});
+      }
+    }
+  
+    // Let's check if the browser supports notifications
+    if (!('Notification' in window)) {
+      console.log("This browser does not support notifications.");
+    } else {
+      if(checkNotificationPromise()) {
+        Notification.requestPermission()
+        .then((permission) => {
+          handlePermission(permission);
+        })
+      } else {
+        Notification.requestPermission(function(permission) {
+          handlePermission(permission);
+        });
+      }
+    }
+  }
+
+  function checkNotificationPromise() {
+    try {
+      Notification.requestPermission().then();
+    } catch(e) {
+      return false;
+    }
+
+    return true;
+  }
+
+
+  var img = '/to-do-notifications/img/icon-128.png';
+  var text = `HEY! You're on deck!!`
+  var notification = new Notification('To do list', { body: text, icon: img });
+
   const generateCode = () => {
     let code = ''
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -43,6 +97,12 @@ export default function App (props) {
           <Main cableApp={props.cableApp}/>
         </Route>
 
+      <button id="enable"
+        onClick={askNotificationPermission}
+        style={permit}
+      >
+        Enable notifications
+        </button>
         
       </div>
     );
