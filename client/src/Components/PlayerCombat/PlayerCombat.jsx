d183f194f2aeb85f336ec819dc2a77e1f6f099bc
Pete Du Beau <peterdubeau@yahoo.com>
2020-12-29 19:56:42 -0500

    rejoin links all work

--- client/src/Components/PlayerCombat/PlayerCombat.jsx
+++ client/src/Components/PlayerCombat/PlayerCombat.jsx
@@ -28,23 +28,19 @@ export default function playerCombat(props) {
   if (onDeckName === props.match.params.username) {
     onDeckAlert()
   }
-  
-  if (props.end === false) {
-    alert("The DM has ended combat")
-    return <Redirect to='/' />
-  }
 
-  if (props.end === 'lobby') {
+
+  if (props.gameData.game.in_combat == false) {
     alert("The DM has ended combat")
     return <Redirect to={`/link/${props.match.params.code}`} />
   }
   
-  if (props.gameData?.combatants === undefined) {
+  if (props.gameData?.combatants.length === undefined) {
     return <Redirect to='/' />
   }
   
-  if (props.end === '') {
-    return <Redirect to={`/link/${props.match.params.code}`}/>
+  if (props.end === 'exit') {
+    return <Redirect to={`/`}/>
   }
 
   function removeCombatant(id) {
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_184112) do
+ActiveRecord::Schema.define(version: 2020_12_29_191431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_10_22_184112) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "combatants", default: [], array: true
+    t.boolean "in_combat", default: false
   end
 
   create_table "users", force: :cascade do |t|
