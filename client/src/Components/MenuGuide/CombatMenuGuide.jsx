0fe9828529538f415ee8c9181d84fa51c22a9123
Pete Du Beau <peterdubeau@yahoo.com>
2020-12-30 11:28:59 -0500

    minor typo in combat tutuorial

--- client/src/Components/MenuGuide/CombatMenuGuide.jsx
+++ client/src/Components/MenuGuide/CombatMenuGuide.jsx
@@ -12,7 +12,7 @@ export default function CombatMenuGuide(props) {
             <p className='combat-guide-close' onClick={() => props.close()}>X</p>
             <p className='combat-guide-info'>
               Clicking the <span id='delete-user'>X</span> next to a PC or enemies name will remove them from combat.<br /><br />
-              When combat is over you can use the <span style={{ color: 'red' }}>End Combat</span> 
+              When combat is over you can <span style={{ color: 'red' }}> End Combat </span> 
               to send all players back to the Lobby, Or you can
             <span style={{ color: 'red' }}> End Game Session </span>
             which will delete this game along with all players.</p>
