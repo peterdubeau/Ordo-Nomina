5fbf55a320eb622e17eee6522c428307b1169c3e
Pete Du Beau <peterdubeau@yahoo.com>
2024-03-01 18:05:30 -0500

    updated secret key base env name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::API
-  SECRET_KEY = Rails.env == 'production' ? ENV['SECRET_KEY'] : Rails.application.secrets.secret_key_base.to_s
+  SECRET_KEY_BASE = Rails.env == 'production' ? ENV['SECRET_KEY_BASE'] : Rails.application.secrets.secret_key_base.to_s
 end
