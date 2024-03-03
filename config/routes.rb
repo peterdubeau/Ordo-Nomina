1290ec0ce27a51e262e8f020109127709825c4b5
Pete Du Beau <peterdubeau@yahoo.com>
2024-03-03 09:15:22 -0500

    more render updates

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,14 @@ Rails.application.routes.draw do
   put 'game/:code/turn', to: 'games#take_turn'
   put 'game/:code/kill', to: 'games#remove_character'
   put 'game/:code/clear', to: 'games#clear_room'
-  
+  get 'render/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  root "render#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
