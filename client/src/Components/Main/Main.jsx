81ac9e5eaa035aaed16d5d3335039c62d5b2cdc1
Pete Du Beau <peterdubeau@yahoo.com>
2024-03-03 09:40:29 -0500

    removed render controller stuff

--- client/src/Components/Main/Main.jsx
+++ client/src/Components/Main/Main.jsx
@@ -1,266 +1,272 @@
-import React, { Component } from 'react';
-import { Route, Redirect } from 'react-router-dom'
-import PlayerLobby from '../PlayerLobby/PlayerLobby'
-import AdminLobby from '../AdminLobby/AdminLobby'
-import PlayerCombat from '../PlayerCombat/PlayerCombat'
-import AdminCombat from '../AdminCombat/AdminCombat'
-import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline'; 
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+import PlayerLobby from "../PlayerLobby/PlayerLobby";
+import AdminLobby from "../AdminLobby/AdminLobby";
+import PlayerCombat from "../PlayerCombat/PlayerCombat";
+import AdminCombat from "../AdminCombat/AdminCombat";
+import ScopedCssBaseline from "@material-ui/core/ScopedCssBaseline";
 
 class Main extends Component {
   constructor(props) {
-    super()
+    super();
     this.state = {
-      inCombat: '',
+      inCombat: "",
       currentGame: {
         game: {},
         users: [],
-        combatants: ''
-      }
-    }
+        combatants: "",
+      },
+    };
   }
-
 
   getGameData = async (id) => {
-    const baseUrl = process.env.NODE_ENV === 'production'
-      ? `https://${process.env.REACT_APP_API_DEPLOYMENT}`
-      : `http://localhost:3000`;
+    const baseUrl =
+      process.env.NODE_ENV === "production"
+        ? `https://${process.env.REACT_APP_API_DEPLOYMENT}`
+        : `http://localhost:3000`;
     await fetch(`${baseUrl}/game/${id}/users`)
-        .then(response => response.json())
-        .then(results => {
-          this.setState({
-            inCombat: results.inCombat,
-            currentGame: {
-              game: results,
-              users: results.users,
-              combatants: results.combatants
-            }
-          })
-        })
-  }
+      .then((response) => response.json())
+      .then((results) => {
+        this.setState({
+          inCombat: results.inCombat,
+          currentGame: {
+            game: results,
+            users: results.users,
+            combatants: results.combatants,
+          },
+        });
+      });
+  };
 
   handleUpClick = (index) => {
     if (index !== 0) {
-      this.setState(prevState => {
-        let list = [...prevState.currentGame.users.filter(user => user.is_admin === false)]
+      this.setState((prevState) => {
+        let list = [
+          ...prevState.currentGame.users.filter(
+            (user) => user.is_admin === false
+          ),
+        ];
         let temp = list[index - 1];
         list[index - 1] = list[index];
         list[index] = temp;
         return {
           currentGame: {
-              ...this.state.currentGame,
-              users: list
-          }
-        }
-      })
+            ...this.state.currentGame,
+            users: list,
+          },
+        };
+      });
     }
-  }
+  };
 
   handleDownClick = (index) => {
     if (index !== this.state.currentGame.users.length - 1) {
-      this.setState(prevState => {
-        let list = [...prevState.currentGame.users]
+      this.setState((prevState) => {
+        let list = [...prevState.currentGame.users];
         let temp = list[index + 1];
         list[index + 1] = list[index];
         list[index] = temp;
         return {
           currentGame: {
             ...this.state.currentGame,
-            users: list
-          }
-        }
-      })
+            users: list,
+          },
+        };
+      });
     }
-  }
-  
-        
+  };
+
   handleSort = () => {
     let list = this.state.currentGame.users.sort(function (a, b) {
-      return b.initiative - a.initiative
-    })
+      return b.initiative - a.initiative;
+    });
     this.setState({
-      users: list
-    })
-  }
-
+      users: list,
+    });
+  };
 
   makeArray = (data) => {
-    let players = []
-    data.forEach(user => {
-        players.push(user.id)
-      })
-    return players
-  }
-
+    let players = [];
+    data.forEach((user) => {
+      players.push(user.id);
+    });
+    return players;
+  };
 
   updateAppStateGame = async (newGame) => {
     if (newGame.type === "new_user") {
-      await newGame
+      await newGame;
       this.setState({
         currentGame: {
           game: newGame.game,
-          users: [...this.state.currentGame.users, newGame.user]
-        }
-      })
+          users: [...this.state.currentGame.users, newGame.user],
+        },
+      });
     } else if (newGame.type === "update_user") {
-      let user = this.state.currentGame.users.findIndex(user => user.id === newGame.user.id)
-      let userUpdate = [...this.state.currentGame.users]
-      userUpdate[user] = { ...userUpdate[user], initiative: newGame.user.initiative, username: newGame.user.username }
+      let user = this.state.currentGame.users.findIndex(
+        (user) => user.id === newGame.user.id
+      );
+      let userUpdate = [...this.state.currentGame.users];
+      userUpdate[user] = {
+        ...userUpdate[user],
+        initiative: newGame.user.initiative,
+        username: newGame.user.username,
+      };
       this.setState({
-        currentGame: { users: userUpdate }
-      })
+        currentGame: { users: userUpdate },
+      });
     } else if (newGame.type === "delete_user") {
-    
-      let user = this.state.currentGame.users.findIndex(user => user.id === newGame.user.id)
-     
-      let users = [...this.state.currentGame.users]
-      users.splice(user, 1)
+      let user = this.state.currentGame.users.findIndex(
+        (user) => user.id === newGame.user.id
+      );
+
+      let users = [...this.state.currentGame.users];
+      users.splice(user, 1);
       this.setState({
         currentGame: {
           users: users,
           code: newGame.code,
-          game: newGame.game
-        }
-      })
-
+          game: newGame.game,
+        },
+      });
     } else if (newGame.type === "take_turn") {
       this.setState({
         currentGame: {
           game: newGame,
           code: newGame.code,
           combatants: newGame.combatants,
-          users: newGame.users
-        }
-      })
-
-    } else if (newGame.type === 'game_start') {
+          users: newGame.users,
+        },
+      });
+    } else if (newGame.type === "game_start") {
       this.setState({
         inCombat: newGame.inCombat,
         currentGame: {
           game: newGame,
           code: newGame.code,
           users: newGame.users,
-          combatants: newGame.combatants
-        }
-      })
-
+          combatants: newGame.combatants,
+        },
+      });
     } else if (newGame.type === "delete_game") {
       this.setState({
-        inCombat: 'exit',
+        inCombat: "exit",
         currentGame: {
           game: {},
           users: [],
-          combatants: ''
-        }
-      })
-    } else if (newGame.type === "remove_guy"){
+          combatants: "",
+        },
+      });
+    } else if (newGame.type === "remove_guy") {
       this.setState({
         inCombat: true,
         currentGame: {
           game: newGame,
           code: newGame.code,
           users: newGame.users,
-          combatants: newGame.combatants
-        }
-      })
+          combatants: newGame.combatants,
+        },
+      });
     } else if (newGame.type === "to_lobby") {
       this.setState({
         inCombat: newGame.inCombat,
         currentGame: {
           game: {},
           users: [],
-          combatants: ''
-        }
-      })
-      window.location.reload()
+          combatants: "",
+        },
+      });
+      window.location.reload();
     }
-  }
+  };
 
   render() {
-   
     return (
       <div className="App">
+        <ScopedCssBaseline style={{ background: "transparent" }}>
+          <Route
+            exact
+            path="/game/:code/DM/:username"
+            render={(props) => {
+              return this.state.currentGame ? (
+                <AdminLobby
+                  {...props}
+                  userList={this.makeArray}
+                  sort={this.handleSort}
+                  arrange={this.handleUpClick}
+                  arrangeDown={this.handleDownClick}
+                  cableApp={this.props.cableApp}
+                  updateApp={this.updateAppStateGame}
+                  getGameData={this.getGameData}
+                  gameData={this.state.currentGame.users}
+                  currentUser={this.state.currentUser}
+                />
+              ) : (
+                <Redirect to="/" />
+              );
+            }}
+          ></Route>
+        </ScopedCssBaseline>
 
-      <ScopedCssBaseline style={{background: "transparent"}}>
-        <Route exact path='/game/:code/DM/:username' render={(props) => {
-          return this.state.currentGame ?
-            (<AdminLobby
-              {...props}
-              userList={this.makeArray}
-              sort={this.handleSort}
-              arrange={this.handleUpClick}
-              arrangeDown={this.handleDownClick}
-              cableApp={this.props.cableApp}
-              updateApp={this.updateAppStateGame}
-              getGameData={this.getGameData}
-              gameData={this.state.currentGame.users}
-              currentUser={this.state.currentUser}
-            />
+        <Route
+          exact
+          path="/combat/:code/DM/:username"
+          render={(props) => {
+            return this.state.currentGame ? (
+              <AdminCombat
+                {...props}
+                code={this.state.currentGame.code}
+                turn={this.takeTurn}
+                cableApp={this.props.cableApp}
+                updateApp={this.updateAppStateGame}
+                getGameData={this.getGameData}
+                users={this.state.currentGame.users}
+                gameData={this.state.currentGame}
+              />
             ) : (
-              <Redirect to='/' />
-            )
-        }}>
-        </Route>
-      </ScopedCssBaseline>
+              <Redirect to="/" />
+            );
+          }}
+        ></Route>
 
-        <Route exact path='/combat/:code/DM/:username' render={(props) => {
-          return this.state.currentGame ?
-            (<AdminCombat
-              {...props}
-              code={this.state.currentGame.code}
-              turn={this.takeTurn}
-              cableApp={this.props.cableApp}
-              updateApp={this.updateAppStateGame}
-              getGameData={this.getGameData}
-              users={this.state.currentGame.users}
-              gameData={this.state.currentGame}
-            />
-
+        <Route
+          exact
+          path="/game/:code/user/:username"
+          render={(props) => {
+            return this.state.currentGame ? (
+              <PlayerLobby
+                {...props}
+                cableApp={this.props.cableApp}
+                updateApp={this.updateAppStateGame}
+                getGameData={this.getGameData}
+                gameData={this.state.currentGame}
+                currentUser={this.state.currentUser}
+                startGame={this.state.inCombat}
+              />
             ) : (
-              <Redirect to='/' />
-            )
-        }}>
-        </Route>
+              <Redirect to="/" />
+            );
+          }}
+        ></Route>
 
-
-
-
-        <Route exact path='/game/:code/user/:username' render={(props) => {
-          return this.state.currentGame ?
-            (<PlayerLobby
-              {...props}
-              cableApp={this.props.cableApp}
-              updateApp={this.updateAppStateGame}
-              getGameData={this.getGameData}
-              gameData={this.state.currentGame}
-              currentUser={this.state.currentUser}
-              startGame={this.state.inCombat}
-            />
+        <Route
+          exact
+          path="/combat/:code/player/:username"
+          render={(props) => {
+            return this.state.currentGame ? (
+              <PlayerCombat
+                {...props}
+                cableApp={this.props.cableApp}
+                updateApp={this.updateAppStateGame}
+                getGameData={this.getGameData}
+                gameData={this.state.currentGame}
+                end={this.state.inCombat}
+              />
             ) : (
-              <Redirect to='/' />
-            )
-        }}>
-        </Route>
-
-
-
-
-        <Route exact path='/combat/:code/player/:username' render={(props) => {
-          return this.state.currentGame ?
-            (<PlayerCombat
-              {...props}
-              cableApp={this.props.cableApp}
-              updateApp={this.updateAppStateGame}
-              getGameData={this.getGameData}
-              gameData={this.state.currentGame}
-              end={this.state.inCombat}
-            />
-
-            ) : (
-              <Redirect to='/' />
-            )
-        }}>
-        </Route>
-
+              <Redirect to="/" />
+            );
+          }}
+        ></Route>
       </div>
     );
   }
